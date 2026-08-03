a52fa4e20fe627968a829b08a2596dea5c9043dd
marvinfpl <marvinfoppolo@gmail.com>
2026-08-03 20:54:18 +0200

    soft margin correction

--- softMarginSvm.py
+++ softMarginSvm.py
@@ -8,27 +8,38 @@ class SoftMarginSvmClassifier():
         self.bias = None
 
     def fit(self, X: np.ndarray, y: np.ndarray)->None:
+        assert np.all(np.isin(y, [-1, 1]))
+
         n_samples, n_features = X.shape
-        y = y.reshape(1, -1).astype(float)
+        y = y.reshape(-1).astype(float)
         
         Q = np.outer(y, y) * X @ X.T
-        q = -np.eye(n_samples)
-        G = np.eye(n_samples)
-        h = -np.eye(n_samples)
+        q = -np.ones(n_samples)
+        G = np.vstack([
+            -np.eye(n_samples),
+            np.eye(n_samples)
+        ])
+        h = np.hstack([
+            np.zeros(n_samples),
+            self.C*np.ones(n_samples)
+        ])
         A = y.reshape(1, -1)
-        b = [0.0]
+        b = np.array([0.0])
 
         sol = solvers.qp(matrix(Q), matrix(q), matrix(G), matrix(h*self.C), matrix(A), matrix(b))
+
+        if sol['status'] != 'optimal':
+            raise RuntimeError(sol['status'])
 
         alpha = np.array(sol['x']).flatten()
 
         support = alpha > 1e-6
         weights = X.T @ (alpha * y)
-        bias = np.mean(y[support] - X.T[support] @ weights)
+        bias = np.mean(y[support] - X[support] @ weights)
 
         self.weights = weights
         self.bias = bias
 
 
     def predict(self, X: np.ndarray)->np.ndarray:
-        return np.sign(X.T @ self.weights + self.bias)
+        return np.sign(X @ self.weights + self.bias)
