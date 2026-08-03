6b327b89fb03f5d181b53dd21c6c067278025908
marvinfpl <marvinfoppolo@gmail.com>
2026-08-03 19:01:56 +0200

    hard margin

--- hardMarginSvm.py
+++ hardMarginSvm.py
@@ -2,7 +2,7 @@ import numpy as np
 from cvxopt import matrix
 from cvxopt import solvers
 
-class SVMClassifier():
+class HardMarginSvmClassifier():
     def __init__(self):
         self.weights = None
         self.bias = None
@@ -12,24 +12,22 @@ class SVMClassifier():
         "Solve an optimisation problem of the form min w,b 1/2 * w * w such that y(w*x + b) >= 1"
 
         n_samples, n_features = X.shape
-        y = y.reshape(1, -1).astype(float)
+        y = y.reshape(-1).astype(float)
         Q = np.outer(y, y) * X @ X.T
         q = -np.ones(n_samples)
         G = -np.eye(n_samples)
         h = np.zeros(n_samples)
-        A = y
-        b = np.zeros(n_samples)
+        A = y.reshape(1, -1)
+        b = [0.0]
 
         sol = solvers.qp(matrix(Q), matrix(q), matrix(G), matrix(h), matrix(A), matrix(b))
         alpha = np.array(sol['x']).flatten()
 
-        weights = X.T @ alpha @ y
-        bias = np.mean(y -  X @ weights)
+        weights = X.T @ (alpha * y)
+        support = alpha > 1e-6
+        bias = np.mean(y[support] -  X[support] @ weights)
         self.weights = weights
         self.bias = bias
 
     def predict(self, X: np.ndarray)->np.ndarray:
-        results = np.sign(X @ self.weights + self.bias)
-        results[results > 0] = 1
-        results[results < 0] = -1
-        return results
+        return np.sign(X @ self.weights + self.bias)
